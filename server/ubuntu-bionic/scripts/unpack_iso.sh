3636902c01e54206fbb0e305026815fdf7f4add9
george <georgekirillov89@gmail.com>
2018-10-07 21:19:49 +0300

    update

--- server/ubuntu-bionic/scripts/unpack_iso.sh
+++ server/ubuntu-bionic/scripts/unpack_iso.sh
@@ -3,7 +3,7 @@
 # Define variables
 BUILD=../iso
 OSVERSION=18.04.1
-IMAGE=ubuntu-$OSVERSION-server-amd64.iso
+IMAGE=../images/origin/ubuntu-$OSVERSION-server-amd64.iso
 TMPDIR="$(mktemp -d)"
 
 # create clean work space
@@ -11,8 +11,8 @@ rm -rf $BUILD/
 mkdir $BUILD/
 
 # Check availability local iso image
-if ! [[ -e "ubuntu-$OSVERSION-server-amd64.iso" ]]; then
-wget http://cdimage.ubuntu.com/releases/$OSVERSION/release/ubuntu-$OSVERSION-server-amd64.iso
+if ! [[ -e "$IMAGE" ]]; then
+wget -P ../images/origin/ http://cdimage.ubuntu.com/releases/$OSVERSION/release/ubuntu-$OSVERSION-server-amd64.iso
 fi
 
 # Mount image & copy files
