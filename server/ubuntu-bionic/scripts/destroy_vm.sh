284154b540f2a3707da7f2797a47f49f297aa74d
georgiy.kirillov <georgiy.kirillov@amaiz.com>
2019-09-20 23:36:51 +0300

    update

--- server/ubuntu-bionic/scripts/destroy_vm.sh
+++ server/ubuntu-bionic/scripts/destroy_vm.sh
@@ -2,9 +2,9 @@
 
 vm=$1
 # останавливаем ВМ и удаляем её
-for i in destroy undefine
+for i in destroy "undefine --nvram"
 do
 virsh -c qemu:///system $i $vm
 done
 # Удаляем её виртуальный жесткий диск
-rm -f /var/lib/libvirt/images/$1.qcow2
+rm -f $HOME/$1.qcow2
